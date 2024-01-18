79d374efe00097c167701543012352ffb25b0bca
kuchanagowtham <kuchanagowtham06@gmail.com>
2024-01-18 14:40:35 +0000

    super over league

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  //   Write your code here.
+
+  <div className="bg-container">
+    <h1>Super Over League</h1>
+    <div className="img-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image-card"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image-card"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
